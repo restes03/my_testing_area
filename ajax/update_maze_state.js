c6c5bfb15e4e83ec4773aedaef4b4436d510b25b
Russell Estes <estex198@gmail.com>
2018-06-28 09:02:56 -0400

    added grid generation

--- ajax/update_maze_state.js
+++ ajax/update_maze_state.js
@@ -6,10 +6,44 @@
  * https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest#Cross-site_XMLHttpRequest
  */
 
+/** Global Variables **/
+
+// Index of cell ID's
+// Made two-dimensional in init()
+var maze_index = [];
+
+// Number of rows and columns contained in the maze
+var rows;
+var cols;
+
+// Stores bitwise values for each cell indicating open doors
+// Made two-dimensional in init()
+var maze_map = [];
+
+
 
 
 function init() {
         makeRequest();
+
+         // get inputs
+    rows = 10;
+    cols = 10;
+
+    // initialize maze map
+    for (var i = 0; i < rows; i++) {
+        maze_index.push(new Array(cols));
+        maze_map.push(new Array(cols));
+        for (var j = 0; j < cols; j++) {
+            maze_index[i][j] = 0;
+            maze_map[i][j] = 0;
+        }
+    }
+
+    // Generate grid
+    gen_maze(rows, cols);
+
+
 };
 
 
@@ -52,5 +86,54 @@ function makeRequest() {
           }
         }
       }
-  }
+  };
 
+
+
+/** Function gen_maze(rows, cols)
+ * Generates a grid of cells which form the fundamental
+ * structure of a maze.
+ * 
+ * @param	rows	Number of rows to include in the grid.
+ * @param	cols	Number of columns to include in the grid.
+ * 
+ * 
+ */
+
+function gen_maze(rows, cols) {
+  // cells are 30px each having left and right borders
+  // of 1px each. Total maze width is then:
+  maze_width = cols * 32;
+
+  // create fill area to insert cells,
+  // then set id & class
+  var maze_container = document.createElement('div');
+  maze_container.setAttribute("id", "maze_container");
+  document.body.appendChild(maze_container);
+  document.body.replaceChild(maze_container, maze_container);
+  maze_container.style.width = maze_width + "px";
+
+  // generate grid having rows x cols
+  for (var i = 0; i < rows; i++) {
+      // Create first row...
+      var row = document.createElement('div');
+      row.setAttribute("class", "row");
+      maze_container.appendChild(row);
+      row.style.width = maze_width + "px";
+
+      // Fill row with cells...
+      for (var j = 0; j < cols; j++) {
+          var temp = document.createElement('span');
+
+          // We want to be able to identify individual cells,
+          // so assign each an ID having form "row# col#"
+          var id_string = i + "x" + j;
+          temp.setAttribute("id", id_string);
+          temp.setAttribute("class", "cell");
+          row.appendChild(temp);
+
+          // Map the maze
+          maze_index[i][j] = id_string;
+      }
+  }
+} // end gen_maze()
