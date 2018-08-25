afdfec25934b2b76b9abc39a7a2f5e2e52efff3a
Russell Estes <32782467+restes03@users.noreply.github.com>
2018-08-25 15:20:30 -0400

    updated regex in isValidURL()
    Fixed bug where regex in isValidURL() was matching sub-strings of valid domains. For example if a valid domain is google.com, a sub-string such as google.co would erroneously match.

--- validator.js
+++ validator.js
@@ -1,17 +1,34 @@
 'use strict';
+/** isValidator.js - Small module for validating URL strings
+ * 
+ * @author: Russell Estes estex198@gmail.com
+ * 
+ */
+
+
+
+// replace with fs read?
 require('dotenv').config({path: '.env'});
+var url = require('url');
+
+
+
+
+
+
 var validator = {
 
     isValidURL : function(url_string) {
-        let regex = new RegExp(/(https:\/\/|http:\/\/|ftp:\/\/){1}([a-z]{1,3})+([\_\-\.]{1}[a-z0-9]+)*\.([a-z]{2,3}){1}/g);
+        let regex = new RegExp(/(https:\/\/|http:\/\/|ftp:\/\/){1}([a-z]{1,3})+([\_\-\.]{1}[a-z0-9]+)*\.([a-z]{2,3}\b){1}/g);
         if (regex.test(url_string)) {
             return true;
         }
         else {
-            console.log(url_string + ' --> Error: isValidURL() requires the following:\n ' + 
-            '\t- Valid web protocol (e.g. https, http, ftp)\n' +
-            '\t- Valid domain extension (e.g. \'.com\', \'.org\', \'.ca\')  \n' + 
-            '\t- Domain names may only include valid characters (e.g. _,-,.,a-z,0-9)');
+            console.log('Error: Invalid URL string: isValidURL() requires the following:\n ' + 
+            '\t- Valid web protocol (limited to: https, http, ftp)\n' +
+            '\t- Valid domain extension (e.g. \'.com\', \'.org\', \'.ca\', etc...)  \n' + 
+            '\t- Domain names may only include the following characters (\'_\',\'-\',\'.\',a-z,0-9)\n' +
+            '\t[' + url_string + '] --> Invalid URL string');
             return false;
         }
     },
@@ -20,29 +37,27 @@ var validator = {
 
     isValidDomain : function(url_string) {
         const DOMAIN_WHITELIST = process.env.DOMAIN_WHITELIST.split(',');
-        let temp = '';
+        let temp;
         let regex = new RegExp(/([a-z]{1,3})+([\_\-\.]{1}[a-z0-9]+)*\.([a-z]{2,3}){1}/g);
         if (regex.test(url_string)) {
-            temp = url_string.match(regex)[0];
-            if (DOMAIN_WHITELIST.includes(temp)) {
+            temp = url.parse(url_string);
+            // temp = url_string.match(regex)[0];
+            if (DOMAIN_WHITELIST.includes(temp.hostname)) {
                 return true;
             } 
             else {
-                console.log('Error: isValidDomain() requires that urls contain a domain that is whitelisted\n' + 
-                            '\t--> ' + url_string + ' [not whitelisted]');
+                console.log('Error: Invalid domain in URL string: isValidDomain() requires that urls contain a domain that is whitelisted\n' + 
+                            '\t ' + temp.hostname + ' --> Not whitelisted');
             }
         }
         else {
-            console.log(url_string + ' --> Error: isValidDomain() requires the following:\n ' + 
+            console.log('Error: Invalid domain in URL string: isValidDomain() requires the following:\n ' + 
                         '\t- Valid domain extension (e.g. \'.com\', \'.org\', \'.ca\')  \n' + 
-                        '\t- Domain names may only include valid characters (e.g. _,-,.,a-z,0-9)');
-            return false;
-
+                        '\t- Domain names may only include valid characters (e.g. _,-,.,a-z,0-9)\n' +
+                        '\t[' + url_string + '] --> Invalid domain');
         }
+        return false;
     }
 }
 
 module.exports = validator;
-
-
-
